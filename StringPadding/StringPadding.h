e4c01d61641bf6cf7b20ccf6cd032c688f9e70cf
Christian Tietze <christian.tietze@gmail.com>
2016-10-18 10:25:57 +0200

    add empty project

---
+++ StringPadding/StringPadding.h
@@ -0,0 +1,21 @@
+//
+//  StringPadding.h
+//  StringPadding
+//
+//  Created by Christian Tietze on 18/10/16.
+//  Copyright © 2016 CleanCocoa. All rights reserved.
+//
+//  Distributed under the MIT License. See LICENSE file for details.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StringPadding.
+FOUNDATION_EXPORT double StringPaddingVersionNumber;
+
+//! Project version string for StringPadding.
+FOUNDATION_EXPORT const unsigned char StringPaddingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringPadding/PublicHeader.h>
+
+
